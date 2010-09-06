9949fdcbee11e88eac26632bf481ba4dbbc5ce1c
jonalv <jonathan.alvarsson@gmail.com>
2010-09-06 11:57:50 +0200

    only pull from bioclipse folder

--- bpull.sh
+++ bpull.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 cd /Users/jonathan/Work/Bioclipse
-PLUGINS=`find * -type d -maxdepth 0`
+PLUGINS=`find bioclipse* -type d -maxdepth 0`
 for PLUGIN in $PLUGINS
 do
     cd "$PLUGIN"; echo "$PLUGIN"; git pull origin; cd -
