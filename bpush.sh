5bbec64992b6b0a0bbaeb983db5803169b5e79df
jonalv <jonathan.alvarsson@gmail.com>
2010-12-20 16:25:03 +0100

    script for pushing to multiple repos

--- bpush.sh
+++ bpush.sh
@@ -1,10 +1,14 @@
-#/bin/bash
+#!/bin/bash
 
 cd /Users/jonathan/Work/Bioclipse
 PLUGINS=`find * -type d -maxdepth 0`
+
 for PLUGIN in $PLUGINS
 do
-    cd "$PLUGIN"; echo "$PLUGIN"; git push bioclipse; cd -
-    echo ""
+   cd "$PLUGIN"
+   BRANCH=`git status | grep "On branch" | sed 's/^.*On branch //'`
+   echo ""
+   echo $PLUGIN
+   git push bioclipse $BRANCH
+   cd ..
 done
-date
